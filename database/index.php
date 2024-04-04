6a4c09eb31a9dfae18848494c83ea8f2d2ea6b06
VeetiSo <v.sorakivi@gmail.com>
2024-04-04 11:15:44 +0300

    user update, delete update

--- database/index.php
+++ database/index.php
@@ -1,55 +1,42 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
-if (isset($_GET['success'])):
-    ?>
-    <aside>
-        <p><?php echo $_GET['success']; ?></p>
-    </aside>
-<?php
-endif;
+session_start();
+require_once __DIR__ . '/inc/header.php';
 ?>
-<section>
-    <form action="insertData.php" method="post">
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title">
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description"></textarea>
-        </div>
-        <div>
-            <input type="submit" value="Save">
-        </div>
-    </form>
-</section>
-<section>
-    <table>
-        <thead>
-        <tr>
-            <th>media_id</th>
-            <th>user_id</th>
-            <th>filename</th>
-            <th>filesize</th>
-            <th>media_type</th>
-            <th>title</th>
-            <th>description</th>
-            <th>created_at</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php require_once 'selectData.php'; ?>
-        </tbody>
-    </table>
-</section>
-</body>
-</html>
+    <section>
+        <h2>Login</h2>
+        <form action="login.php" method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </section>
+    <section>
+        <h2>Register</h2>
+        <form action="register.php" method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div>
+                <input type="submit" value="Register">
+            </div>
+    </section>
+<?php
+require_once __DIR__ . '/inc/footer.php';
+?>
